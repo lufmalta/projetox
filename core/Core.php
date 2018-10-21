e649ee3f5afc824c12b88a753df4feb81d1442c8
Luiz Fernando <lufmalta@gmail.com>
2018-10-21 18:29:16 -0300

    inserindo o params no core

--- core/Core.php
+++ core/Core.php
@@ -6,6 +6,7 @@ class Core{
     	if(isset($_GET['url'])){
     		$url .= $_GET['url'];
     	}
+    	$params = array();
     	if(!empty($url) && $url != '/'){
     		$url = explode('/', $url);
     		array_shift($url); // retira o primeiro valor do array
@@ -16,16 +17,21 @@ class Core{
     		//print_r($url);
     		if(isset($url[0]) && !empty($url[0])){
     			$currentAction = $url[0];
+    			array_shift($url);
     		}else{
     			$currentAction = "index";
+    		}
+    		if(count($url) > 0){
+    			$params = $url;
     		}
     	}else{
     		$currentController = "homeController";
     		$currentAction = "index";
     	}
 
-    	echo "Controller: ".$currentController;
-    	echo "<br/>Action: ".$currentAction;
+    	echo "Controller: ".$currentController."<br/>";
+    	echo "Action: ".$currentAction."<br/>";
+    	echo "Parametros:".print_r($params, true);
 
     }
 }
